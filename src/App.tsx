8d7cf511f8fa5b84e103ae86d2271b29e1b67a0a
apsuma <delphine.belet@gmail.com>
2020-09-15 18:13:14 +0200

    add navigation and page404

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React, { FunctionComponent} from 'react';
-import PokemonList from './pages/pokemon-list'
-  
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PokemonList from './pages/pokemon-list';
+import PokemonDetail from './pages/pokemon-detail';
+import PageNotFound from './pages/page-not-found';
+
 const App: FunctionComponent = () => {
   return (
-     <PokemonList />
+    <Router>
+      <div>
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">Pokedex</Link>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route path="/pokemons/:id" component={PokemonDetail} />
+          <Route component={PageNotFound}/>
+        </Switch>
+      </div>
+
+    </Router>
  )
 }
   
