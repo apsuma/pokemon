9c4988a83f950b5f0a0442aa5c295fd23fc0c41e
apsuma <delphine.belet@gmail.com>
2020-09-15 14:25:17 +0200

    list of pokemon cards with materialize

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,26 @@ const App: FunctionComponent = () => {
  
     
  return (
-  <div>
+  <div className="container">
     <h1>Pokedex</h1>
-    <h1>Bonjour, nombre de pokemons dans le Pokedex  : {pokemons.length}</h1>
+    <h4>Bonjour, nombre de pokemons dans le Pokedex  : {pokemons.length} </h4>
+    <h2>Liste des pokemons:</h2>
+    <ul className="row">
+        { pokemons.map((pokemon) => (
+        <div className="col s2">
+            <li key={pokemon.id} className="card">
+                <div className="card-image z-depth-4 circle responsive-img">
+                    <img  src={pokemon.picture} alt={pokemon.name}/>
+                </div>
+                <div className="card-content">
+                    <span className="card-title black-text">{pokemon.name}</span>
+                    <p >{pokemon.types}</p>
+                </div>
+            </li>
+        </div>
+        ))}
+
+    </ul>
   </div>
  )
 }
