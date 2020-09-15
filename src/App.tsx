6fc057c8abd51bb4305255d53505ccf4bdc86ca6
apsuma <delphine.belet@gmail.com>
2020-09-15 17:15:56 +0200

    organize code in components, hooks and helpers

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import React, { FunctionComponent, useState, useEffect} from 'react';
-import Pokemon from "./models/pokemon"
-import POKEMONS from './models/mock-pokemon'
+import React, { FunctionComponent} from 'react';
+import PokemonList from './pages/pokemon-list'
   
 const App: FunctionComponent = () => {
- 
- const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
- useEffect(() => {
-     setPokemons(POKEMONS);
- }, []);   
-
- 
-    
- return (
-  <div className="container">
-    <h1>Pokedex</h1>
-    <h4>Bonjour, nombre de pokemons dans le Pokedex  : {pokemons.length} </h4>
-    <h2>Liste des pokemons:</h2>
-    <ul className="row">
-        { pokemons.map((pokemon) => (
-        <div className="col s2">
-            <li key={pokemon.id} className="card">
-                <div className="card-image z-depth-4 circle responsive-img">
-                    <img  src={pokemon.picture} alt={pokemon.name}/>
-                </div>
-                <div className="card-content">
-                    <span className="card-title black-text">{pokemon.name}</span>
-                    <p >{pokemon.types}</p>
-                </div>
-            </li>
-        </div>
-        ))}
-
-    </ul>
-  </div>
+  return (
+     <PokemonList />
  )
 }
   
---
+++ src/pages/pokemon-list.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent} from 'react';
+import usePokemons from '../hooks/pokemon.hook';
+import PokemonCard from '../components/pokemon-card';
+
+const PokemonList: FunctionComponent = () => {
+    const pokemons = usePokemons();  
+  
+ return (
+  <div>
+    <h1 className="center">Pokedex</h1>
+    <div className="container">
+        <div className="row">
+            { pokemons.map((pokemon) => (
+                < PokemonCard key={pokemon.id} pokemon={pokemon}/>
+            ))};
+        </div>
+    </div>
+  </div>
+ );
+}
+
+export default PokemonList;
