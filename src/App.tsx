8b551776540817ec9090f21b5fbeb68c840fba85
apsuma <delphine.belet@gmail.com>
2020-09-16 12:06:06 +0200

    add Editpage, editForm, state initialization

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PokemonList from './pages/pokemon-list';
 import PokemonDetail from './pages/pokemon-detail';
 import PageNotFound from './pages/page-not-found';
+import PokemonEdit from './pages/pokemon-edit';
 
 const App: FunctionComponent = () => {
   return (
@@ -16,6 +17,7 @@ const App: FunctionComponent = () => {
         <Switch>
           <Route exact path="/" component={PokemonList} />
           <Route exact path="/pokemons" component={PokemonList} />
+          <Route exact path="/pokemons/edit/:id" component={PokemonEdit} />
           <Route path="/pokemons/:id" component={PokemonDetail} />
           <Route component={PageNotFound}/>
         </Switch>
